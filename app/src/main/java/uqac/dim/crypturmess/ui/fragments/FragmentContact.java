3e24fd127abc6dafabac0f25a5baa8d8c00c40e8
ulysse-neo <u-neo.lartigaud@etu.uca.fr>
2022-04-05 23:46:09 +0200

    Mise en place de FragmentContact

--- app/src/main/java/uqac/dim/crypturmess/ui/fragments/FragmentContact.java
+++ app/src/main/java/uqac/dim/crypturmess/ui/fragments/FragmentContact.java
@@ -2,20 +2,31 @@ package uqac.dim.crypturmess.ui.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.fragment.app.ListFragment;
+import androidx.room.RoomDatabase;
+import androidx.lifecycle.*;
 
+import java.util.List;
+
+import uqac.dim.crypturmess.CrypturMessApplication;
 import uqac.dim.crypturmess.R;
+import uqac.dim.crypturmess.databaseAccess.room.AppLocalDatabase;
+import uqac.dim.crypturmess.model.entity.Conversation;
+import uqac.dim.crypturmess.model.entity.Message;
+import uqac.dim.crypturmess.model.entity.User;
 
 public class FragmentContact extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), , android.R.layout.simple_list_item_1);
+        //ERROR : java.lang.IllegalStateException: Cannot access database on the main thread since it may potentially lock the UI for a long period of time.
+        ArrayAdapter<User> adapter = new ArrayAdapter<User>(getActivity(), android.R.layout.list_content, AppLocalDatabase.getInstance(CrypturMessApplication.getContext()).userDao().getFriends());
         setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);*/
+
     }
 
     @Override
