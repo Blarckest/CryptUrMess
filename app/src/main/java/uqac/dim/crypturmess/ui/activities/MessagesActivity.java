2d602c71a41e524530588e4de3d1d44fbbadb15e
Thomas <devienne.thomas@hotmail.fr>
2022-04-19 23:51:26 -0400

    Ajout d'un bouton pour récupérer les coordonnées GPS de l'utilisateur à partir du réseau

--- app/src/main/java/uqac/dim/crypturmess/ui/activities/MessagesActivity.java
+++ app/src/main/java/uqac/dim/crypturmess/ui/activities/MessagesActivity.java
@@ -1,16 +1,20 @@
 package uqac.dim.crypturmess.ui.activities;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,17 +24,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.sql.Timestamp;
-
 import uqac.dim.crypturmess.CrypturMessApplication;
 import uqac.dim.crypturmess.R;
 import uqac.dim.crypturmess.databaseAccess.SharedPreferencesHelper;
 import uqac.dim.crypturmess.databaseAccess.firebase.FirebaseHelper;
 import uqac.dim.crypturmess.databaseAccess.room.AppLocalDatabase;
-import uqac.dim.crypturmess.model.entity.Conversation;
 import uqac.dim.crypturmess.model.entity.CryptedMessage;
 import uqac.dim.crypturmess.model.entity.Message;
-import uqac.dim.crypturmess.model.entity.UserClientSide;
 import uqac.dim.crypturmess.ui.adapter.MessageListAdapter;
 import uqac.dim.crypturmess.ui.notifications.Notifier;
 import uqac.dim.crypturmess.utils.crypter.AES.AESDecrypter;
@@ -47,9 +47,9 @@ public class MessagesActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private FirebaseHelper firebaseHelper = new FirebaseHelper();
     private DatabaseReference firebaseDB = FirebaseDatabase.getInstance().getReference();
-    private SharedPreferencesHelper sharedPreferencesHelper=new SharedPreferencesHelper();
-    private IDecrypter RSADecrypter=new RSADecrypter();
-    private IDecrypter AESDecrypter=new AESDecrypter();
+    private SharedPreferencesHelper sharedPreferencesHelper = new SharedPreferencesHelper();
+    private IDecrypter RSADecrypter = new RSADecrypter();
+    private IDecrypter AESDecrypter = new AESDecrypter();
     private MessageListAdapter adapter;
 
 
@@ -78,7 +78,7 @@ public class MessagesActivity extends AppCompatActivity {
                         if (snapshot.getValue() != null) {
                             CryptedMessage msgCrypte = snapshot.getValue(CryptedMessage.class);
                             Message msg = null;
-                            if (msgCrypte.getAlgorithm()!=null){
+                            if (msgCrypte.getAlgorithm() != null) {
                                 if (msgCrypte.getAlgorithm().equals(Algorithm.RSA))
                                     msg = new Message(msgCrypte, RSADecrypter, false, true);
                                 else if (msgCrypte.getAlgorithm().equals(Algorithm.AES))
@@ -147,4 +147,66 @@ public class MessagesActivity extends AppCompatActivity {
         ((RecyclerView) findViewById(R.id.m_recycle)).scrollToPosition(adapter.getItemCount() - 1);
     }
 
+    /**
+     * Get current location of the phone
+     */
+    public void onGetLocation(View view) {
+        setUserLocation();
+    }
+
+    public void setUserLocation() {
+        // Creating an instance of LocationManager in the context of LOCATION_SERVICE.
+        //Location currentLocation = null;
+        LocationManager locationManager;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Check that if the GPS and Network are available or not and if both are available then we use one with greater accuracy.
+        /*boolean hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        String provider;
+        if (hasGps)
+            provider = LocationManager.GPS_PROVIDER;
+        else
+            provider = LocationManager.NETWORK_PROVIDER;*/
+
+        String provider = LocationManager.NETWORK_PROVIDER;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        String coord = Double.toString(location.getLatitude()) + Double.toString(location.getLongitude());
+        ((EditText) findViewById(R.id.m_enter_message)).setText(coord);
+        Log.i("LOCATION", coord);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // On relance la méthode de réalisation de l'action. Cette fois, on a l'autorisation.
+                    setUserLocation();
+
+                } else {
+                   Toast.makeText(getApplicationContext(), "Impossible de récupérer la locatlisation actuellement", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
 }
