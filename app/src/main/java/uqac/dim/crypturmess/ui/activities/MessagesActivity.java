f24517447cc59b73eaad221fd3852d5d689627e8
Thomas <devienne.thomas@hotmail.fr>
2022-04-20 01:38:11 -0400

    Essai d'ajout de regex pour la geolocalisation (non encore fonctionnelle)

--- app/src/main/java/uqac/dim/crypturmess/ui/activities/MessagesActivity.java
+++ app/src/main/java/uqac/dim/crypturmess/ui/activities/MessagesActivity.java
@@ -2,11 +2,14 @@ package uqac.dim.crypturmess.ui.activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,6 +26,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import uqac.dim.crypturmess.CrypturMessApplication;
 import uqac.dim.crypturmess.R;
@@ -64,7 +70,6 @@ public class MessagesActivity extends AppCompatActivity {
             id_user = extras.getString("ID_USER");
         }
 
-
         recyclerView = (RecyclerView) findViewById(R.id.m_recycle);
         adapter = new MessageListAdapter(this, AppLocalDatabase.getInstance(CrypturMessApplication.getContext()).messageDao().getAllMessagesByConvId(id));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -73,18 +78,30 @@ public class MessagesActivity extends AppCompatActivity {
 
         firebaseDB.child("messages").child(sharedPreferencesHelper.getValue(R.string.userIDSharedPref)).child(id_user).addChildEventListener(
                 new ChildEventListener() {
+                    Pattern p = Pattern.compile("[+-]?[0-9]{2}\\.[0-9]{7} ; [+-]?[0-9]{2}\\.[0-9]{7}");
+                    Matcher m;
                     @Override
                     public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                         if (snapshot.getValue() != null) {
                             CryptedMessage msgCrypte = snapshot.getValue(CryptedMessage.class);
                             Message msg = null;
                             if (msgCrypte.getAlgorithm() != null) {
-                                if (msgCrypte.getAlgorithm().equals(Algorithm.RSA))
+                                if (msgCrypte.getAlgorithm().equals(Algorithm.RSA)) {
                                     msg = new Message(msgCrypte, RSADecrypter, false, true);
-                                else if (msgCrypte.getAlgorithm().equals(Algorithm.AES))
+                                    m = p.matcher((CharSequence) msg);
+                                    Toast.makeText(getApplicationContext(), (CharSequence) msg, Toast.LENGTH_LONG).show();
+                                }
+                                else if (msgCrypte.getAlgorithm().equals(Algorithm.AES)) {
                                     msg = new Message(msgCrypte, AESDecrypter, false, true);
+                                    m = p.matcher((CharSequence) msg);
+                                    Toast.makeText(getApplicationContext(), (CharSequence) msg, Toast.LENGTH_LONG).show();
+                                }
                                 else
                                     Log.e("DIM", "onChildAdded: Bad algorithm while receiving");
+
+                                if (m.find()) {
+                                    Toast.makeText(getApplicationContext(), "Coucou la zone", Toast.LENGTH_LONG).show();
+                                }
                                 adapter.addMessage(msg);
                                 ((RecyclerView) findViewById(R.id.m_recycle)).scrollToPosition(adapter.getItemCount() - 1);
                             }
@@ -154,7 +171,7 @@ public class MessagesActivity extends AppCompatActivity {
         setUserLocation();
     }
 
-    public void setUserLocation() {
+    public Pair<Double, Double> getUserLocation() {
         // Creating an instance of LocationManager in the context of LOCATION_SERVICE.
         //Location currentLocation = null;
         LocationManager locationManager;
@@ -180,13 +197,25 @@ public class MessagesActivity extends AppCompatActivity {
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
+            return null;
         }
         Location location = locationManager.getLastKnownLocation(provider);
-        String coord = Double.toString(location.getLatitude()) + Double.toString(location.getLongitude());
+        return new Pair<Double, Double>(location.getLatitude(), location.getLongitude());
+    }
+
+    public void setUserLocation() {
+        Pair<Double, Double> pcoord = getUserLocation();
+        String coord = Double.toString(pcoord.first) + " ; " + Double.toString(pcoord.second);
         ((EditText) findViewById(R.id.m_enter_message)).setText(coord);
         Log.i("LOCATION", coord);
+    }
 
+    public void openInMaps(View view) {
+        Pair<Double, Double> pcoord = getUserLocation();
+        Uri navigationIntentUri = Uri.parse("google.navigation:q=" + Double.toString(pcoord.first) +"," + Double.toString(pcoord.second));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, navigationIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 
     @Override
@@ -208,4 +237,10 @@ public class MessagesActivity extends AppCompatActivity {
             }
         }
     }
+
+    /*
+    regex pour match les coordonnées GPS
+    2.7
+    ([0-9]{2}\.[0-9]{7}) ; ([0-9]{2}\.[0-9]{7})
+     */
 }
