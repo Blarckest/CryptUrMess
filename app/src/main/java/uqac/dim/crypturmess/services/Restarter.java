8fb17585ef1cf1f3b4858676c119e6f4d25ff451
yogourves <yoann.gourves@gmail.com>
2022-04-26 22:11:02 -0400

    c bon

--- app/src/main/java/uqac/dim/crypturmess/services/Restarter.java
+++ app/src/main/java/uqac/dim/crypturmess/services/Restarter.java
@@ -5,10 +5,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import uqac.dim.crypturmess.databaseAccess.firebase.FirebaseHelper;
+import uqac.dim.crypturmess.utils.auth.FirebaseAuthManager;
+import uqac.dim.crypturmess.utils.auth.IAuthManager;
+
 public class Restarter extends BroadcastReceiver {
+    private IAuthManager authManager=new FirebaseAuthManager();
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("DIM", "Service tried to stop,restarted");
-        context.startForegroundService(new Intent(context, AppService.class));
+        if (authManager.getCurrentUser()!=null) {
+            authManager.getCurrentUser().reload().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    if (authManager.getCurrentUser() != null) {
+                        context.startForegroundService(new Intent(context, AppService.class));
+                    }
+                }
+            });
+        }
     }
 }
